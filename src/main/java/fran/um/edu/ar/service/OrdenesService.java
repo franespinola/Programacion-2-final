67313115a424850d55071c67be3f227939f81ffb
francisco espinola <franespinola_92@hotmail.com>
2023-11-16 11:41:42 -0300

    agregue OrdenService

---
+++ src/main/java/fran/um/edu/ar/service/OrdenesService.java
@@ -0,0 +1,13 @@
+package fran.um.edu.ar.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class OrdenesService {
+
+    private final Logger log = LoggerFactory.getLogger(OrdenesService.class);
+}
